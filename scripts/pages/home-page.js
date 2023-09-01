aa92b6cd1b8288ec0d2e7f4b64280958661c467e
“Kamaqen” <jmcapcha@gmail.comgit config --global core.autocrlf inputgit config --global core.safecrlf truegit config --global core.editor code>
2023-08-31 20:35:29 -0500

    ADD: missing images to home page

--- scripts/pages/home-page.js
+++ scripts/pages/home-page.js
@@ -27,8 +27,8 @@ function render() {
       <main>
         <section class="container">
           <div class="header-home">
-          <img class="add-link" src="/assets/images/{ doable }.png" alt="" srcset="">
-          <a class="logout-link" href="#">Logout</a>
+            <img class="add-link" src="/assets/images/{ doable }.png" alt="doable logo" srcset="">
+            <img class="logout-link" src="/assets/images/logout-icon.svg" alt="logout icon" srcset="">
           </div>
           ${Tasks}
           <div class="footer js-links-pages">
