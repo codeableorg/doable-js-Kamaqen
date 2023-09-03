7e5ffa742a8b2975f197b53dd3d04c812e5df487
“Kamaqen” <jmcapcha@gmail.comgit config --global core.autocrlf inputgit config --global core.safecrlf truegit config --global core.editor code>
2023-09-02 19:24:07 -0500

    FIX: submit button not working

--- scripts/components/tasks.js
+++ scripts/components/tasks.js
@@ -3,13 +3,13 @@ import STORE from "../store.js";
 
 function renderTask(task) {
     return `
-  <li class="form-fields flex justify-between tasks-list">
+  <li class="flex justify-between">
     <div class="">
       <input class="checkbox checkbox__input" type="checkbox">
     </div>
     <div class="flex flex-column">
-      <p class="js-link-task" data-id=${task.id} >${task.title}</p>
-      <p class="js-link-task" data-id="" >${task.due_date || ""}</p>
+      <p class="" data-id=${task.id} >${task.title}</p>
+      <p class="" data-id="" >${task.due_date || ""}</p>
     </div>
     <div class="">
       <img id=${task.id} class="" src="/assets/images/important-false.png" />
